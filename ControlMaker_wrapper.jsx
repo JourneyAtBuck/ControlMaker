9a1c2048c6c47a096bc7bdedafb3283ddcd3d1c2
Moses Journey <journey@buck.tv>
2015-11-24 22:17:57 -0800

    wrapper for testing

---
+++ ControlMaker_wrapper.jsx
@@ -0,0 +1,9 @@
+﻿{
+    //Note: this file must be named "BuckTab.jsx" for the docked palette to have the correct name
+    var libfile = File('C:/Users/Moses Journey/Documents/GitHub/ControlMaker/ControlMaker.jsx');
+
+    try{
+        $.evalFile(libfile);
+        MJ_ControlMaker(this);
+    } catch(err){alert("Script file missing.\rError:"+err)}
+}
